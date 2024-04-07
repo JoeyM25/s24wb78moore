41440f0510e73ddb2dc057aa82b23aac9664a610
JoeyM25 <63532048+JoeyM25@users.noreply.github.com>
2024-04-07 00:21:22 -0500

    Added random item

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var houseplantRouter = require('./routes/houseplant')
 var gridRouter = require('./routes/grid')
+var randRouter = require('./routes/pick')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/houseplant', houseplantRouter)
 app.use('/grid', gridRouter)
+app.use('/randomitem', randRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
